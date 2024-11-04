653e484f1feda116959553ebdc50b3aca76db233
Misha <98160546+Msho19972010@users.noreply.github.com>
2024-11-04 19:58:09 +0100

    Optimised methods
    Minimized some parts of my codes by using generics, deleted a custom exception and used a default one and accelerated theSecondLargestValue method.

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -2,42 +2,42 @@ import java.util.*;
 
 public class Main{
 
-    public static void main(String[] args) throws EmptyCollectionException {
+    public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1,2,3,4,4,4,4,5,5,5,6,7,8,9,1,2,3);
+        List<String> words = Arrays.asList("nana", "nomanm", "bird", "car", "bird", "film", "name");
+        List<Integer> numbers2 = Arrays.asList(14,25,36,41,42,43,44,55,512,52342,62,799,88,987,1,2,3);
+
         System.out.println("-This is the list without duplicates: " + deleteDuplicates(numbers));
 
-        List<String> words = Arrays.asList("nana", "nomanm", "bird", "car", "film", "name");
+        System.out.println("-This is the list without duplicates: " + deleteDuplicates(words));
+
         System.out.println("-The count of specific letter that is beginning of words: " + countStringsStartingWith(words));
 
-        List<Integer> numbers2 = Arrays.asList(14,25,36,41,42,43,44,55,512,52342,62,799,88,987,1,2,3);
         System.out.println("-The second-largest element in the  list is: " + secondLargestElement(numbers2));
     }
 
-    public static Collection deleteDuplicates(Collection list) throws EmptyCollectionException {
+    public static <T>Collection deleteDuplicates (Collection<T> list) {
         if(list.isEmpty()) {
-            throw new EmptyCollectionException();
+            throw new NoSuchElementException();
         }
 
-        if(list.stream().allMatch(String.class::isInstance)) {
-            return list.stream().distinct().toList();
-        } else {
-            return list.stream().distinct().toList();
-        }
+        return list.stream().distinct().toList();
     }
-    public static int countStringsStartingWith(List<String> list) throws EmptyCollectionException{
+    public static int countStringsStartingWith(List<String> list) {
         if(list.isEmpty()) {
-            throw new EmptyCollectionException();
+            throw new NoSuchElementException();
         }
 
         return (int) list.stream().filter(s -> s.startsWith("n")).count();
     }
 
-    public static int secondLargestElement(List<Integer> list) throws EmptyCollectionException{
-        if(list.isEmpty()) {
-            throw new EmptyCollectionException();
+    public static int secondLargestElement(List<Integer> list) {
+        if(list.size() < 2) {
+            throw new NoSuchElementException();
         }
 
-        return list.stream().sorted().skip(list.size() - 2).findFirst().orElseThrow();
-    }
+        Optional<Integer> maxValue = list.stream().max(Integer::compare);
 
+        return list.stream().filter(i -> i < maxValue.get()).toList().stream().max(Integer::compare).orElse(0);
+    }
 }
